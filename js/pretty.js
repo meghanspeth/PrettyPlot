86145d5f773b341b01c7cb448aec8725634e5b71
Meghan Speth <meghanspeth@Meghans-MacBook-Pro.local>
2020-09-25 15:29:55 -0400

    finished lab

--- js/pretty.js
+++ js/pretty.js
@@ -70,8 +70,19 @@ var makeTranslateString = function(x,y)
 var drawAxes = function(graphDim,margins,
                          xScale,yScale)
 {
-   
- 
+  var xAxis= d3.axisBottom(xScale)
+  var yAxis=d3.axisLeft(yScale)
+  
+    d3.select("svg")
+        .append("g")
+         .attr("transform","translate("+margins.left+","+(margins.top+graphDim.height)+")")
+        .call(xAxis)
+    
+    d3.select("svg")
+        .append("g")
+         .attr("transform","translate("+margins.left+"," +(margins.top)+")")
+        .call(yAxis)
+        
 }
 
 
@@ -79,15 +90,43 @@ var drawAxes = function(graphDim,margins,
 //margins - objedct that stores the size of the margins
 var drawLabels = function(graphDim,margins)
 {
+    var Labels= d3.select("svg")
+        .append("g")
+        .classed("labels",true)
     
+        Labels.append("text")
+               .text("Support For Trump")
+                .classed("title", true)
+                .attr("text-anchor","middle")
+                .attr("x",margins.left+(graphDim.width/2))
+                .attr("y",margins.top*1.5)
+    
+        Labels.append("text")
+                .text("Percent White")
+                .classed("label",true)
+                .attr("text-anchor","middle")
+                .attr("x", margins.left+(graphDim.width/2))
+                .attr("y",screen.height*.75)
+    
+     Labels.append("g")
+            .attr("transform","translate(15,"+(margins.top+(graphDim.height/2))+")")
+            .append("text")
+            .text("Percentage Voting for Trump")
+            .classed ("label",true)
+            .attr("text-anchor","middle")
+            .attr("transform","rotate(270)")
+      
 }
 
 
 var drawLegend = function(graphDim,margins)
 {
+    var Legend = d3.select("svg")
+        .append("g")
+        .classed("legend", true)
+        .attr("transform","translate("+(margins.left+10) +"," + (margins.top+10)+")");
     
- 
-   var categories = [
+    var categories = [
        {
            class:"lessCollege",
            name:"Less College"
@@ -97,6 +136,31 @@ var drawLegend = function(graphDim,margins)
            name:"High unemployment"
        }
     ]
+    
+    var entries=Legend.selectAll("g")
+        .data(categories)
+        .enter()
+        .append("g")
+        .classed("legendEntry",true)
+        .attr("class", function(category)
+             {
+            return category.class;
+        })
+        .attr("transform", function(category,index)
+             {
+                return "translate(0,"+(index*20)+")";
+        })
+ 
+    entries.append("rect")
+            .attr("width",10)
+            .attr("height",10)
+    
+    entries.append("text")
+            .text(function(category)
+                 {return category.name})
+            .attr("x",15)
+            .attr("y",10)
+  
 
 
     
@@ -108,9 +172,9 @@ var drawLegend = function(graphDim,margins)
 var initGraph = function(counties)
 {
     //size of screen
-    var screen = {width:800,height:600}
+    var screen = {width:870,height:600}
     //how much space on each side
-    var margins = {left:30,right:20,top:20,bottom:30}
+    var margins = {left:50,right:20,top:20,bottom:40}
     
     
     
@@ -145,10 +209,7 @@ var initGraph = function(counties)
     drawPlot(counties,target,xScale,yScale);
     drawLabels(graph,margins);
     drawLegend(graph,margins);
-    
-    
-    
-    
+      
     
 }
 
@@ -168,3 +229,6 @@ var failFCN = function(error)
 
 var polPromise = d3.csv("data/politics.csv")
 polPromise.then(successFCN,failFCN);
+
+
+   
